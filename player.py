b02ba6d8993c48f6bcc10396fa8231912c8f41c9
Monica Banda <monicabanda@Monicas-Air.lan>
2023-03-20 17:00:30 -0600

    Added Code to Player File

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self):
+        self.gestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+        self.name = ""
+        self.score = 0
+        self.chosen_gesture =""
+
+
+    def choose_gesture(self):
+        pass
